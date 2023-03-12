3dcb63c24fd2802581e9744533708a4a8373eb7e
Erlan Rangel <erlanrangel@gmail.com>
2023-03-11 20:37:20 -0600

    [feat] check dirs before implementing

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,94 @@
+use std::env;
 use std::fs;
+use std::io::{self, Write};
+use std::path::{Path, PathBuf};
 
 fn main() {
-    // Ask the user for a PCN name
-    println!("Enter a PCN name:");
-    let mut pcn_name = String::new();
-    std::io::stdin().read_line(&mut pcn_name).unwrap();
-
-    // Remove any whitespace or newline characters from the input
-    pcn_name = pcn_name.trim().to_string();
-
-    // Create the PCN directory if it doesn't already exist
-    if let Err(_) = fs::create_dir(&pcn_name) {
-        println!("Warning: Directory '{}' already exists", pcn_name);
-    } else {
-        println!("Directory '{}' created successfully", pcn_name);
-    }
-
-    // Ask the user for comma-separated directory names
-    println!("Enter directory names (comma-separated):");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-
-    // Remove any whitespace or newline characters from the input
-    input = input.trim().to_string();
-
-    // Split the input into individual directory names
-    let dirs: Vec<&str> = input.split(',').map(|s| s.trim()).collect();
-
-    // Create each directory inside the PCN directory
-    for dir in dirs {
-        let path = format!("{}/{}", pcn_name, dir);
-        if let Err(_) = fs::create_dir(&path) {
-            println!("Warning: Directory '{}' already exists", path);
-        } else {
-            println!("Directory '{}' created successfully", path);
-        }
+    let pcn_path = get_pcn_path();
+    ensure_directory_exists(&pcn_path);
+    let pcn_number = get_pcn_number();
+    let pcn_number_path = pcn_path.join(&pcn_number);
+    ensure_directory_exists(&pcn_number_path);
+    let affected_part_numbers = get_affected_part_numbers();
+    for part_number in &affected_part_numbers {
+        let part_number_path = pcn_number_path.join(&part_number);
+        ensure_directory_exists(&part_number_path);
+        copy_and_rename_file(
+            &pcn_path.join("PCN_template.xlsx"),
+            &part_number_path.join(&part_number).with_extension("xlsx"),
+        )
+        .expect("Failed to copy and rename file");
     }
 }
+
+fn get_pcn_path() -> PathBuf {
+    if is_windows() {
+        let documents_path =
+            env::var("USERPROFILE").expect("USERPROFILE environment variable not found.");
+        PathBuf::from(documents_path).join("Documents").join("PCN")
+    } else {
+        env::current_dir()
+            .expect("Failed to get current working directory")
+            .join("PCN")
+    }
+}
+
+fn ensure_directory_exists(path: &Path) {
+    if !is_directory(path) {
+        create_directory(path);
+    }
+}
+
+fn get_pcn_number() -> String {
+    print!("Please enter the PCN number: ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input.trim().to_string()
+}
+
+fn get_affected_part_numbers() -> Vec<String> {
+    print!("Please enter a comma-separated list of affected part numbers: ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input
+        .trim()
+        .split(',')
+        .map(|s| s.trim().to_string())
+        .collect()
+}
+
+fn is_windows() -> bool {
+    std::env::consts::OS == "windows"
+}
+
+fn is_directory(path: &Path) -> bool {
+    fs::metadata(path)
+        .map(|metadata| metadata.is_dir())
+        .unwrap_or(false)
+}
+
+fn create_directory(path: &Path) {
+    fs::create_dir(path).expect("Failed to create directory");
+}
+
+fn copy_and_rename_file(src: &Path, dest: &Path) -> io::Result<()> {
+    let mut contents = fs::read(src)?;
+    let parent_dir_name = dest
+        .parent()
+        .unwrap()
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap();
+    let dest_with_name = dest.with_file_name(parent_dir_name).with_extension("xlsx");
+    contents.shrink_to_fit();
+    fs::write(&dest_with_name, contents)?;
+    Ok(())
+}
+
--- src/test.rs
+++ src/test.rs
@@ -1,45 +1,73 @@
 #[cfg(test)]
-
 mod tests {
     use super::*;
-    use std::fs;
+    use std::fs::{self, File};
+    use std::io::Write;
+    use tempfile::TempDir;
 
     #[test]
-    fn test_create_pcn_dir() {
-        let pcn_name = "test_pcn";
-        let dir_path = format!("./{}", pcn_name);
-
-        // Test creating a PCN directory that doesn't already exist
-        assert!(create_pcn_dir(pcn_name).is_ok());
-        assert!(fs::metadata(&dir_path).unwrap().is_dir());
-
-        // Test creating a PCN directory that already exists
-        assert!(create_pcn_dir(pcn_name).is_err());
+    fn test_create_pcn_directory() {
+        let temp_dir = TempDir::new().unwrap();
+        let pcn_path = temp_dir.path().join("PCN");
+        ensure_directory_exists(&pcn_path);
+        assert!(is_directory(&pcn_path));
     }
 
     #[test]
-    fn test_create_subdirs() {
-        let pcn_name = "test_pcn";
-        let dir_path = format!("./{}", pcn_name);
+    fn test_get_pcn_path_on_windows() {
+        let user_profile = "C:\\Users\\TestUser";
+        env::set_var("USERPROFILE", user_profile);
+        assert_eq!(
+            get_pcn_path(),
+            PathBuf::from(user_profile).join("Documents").join("PCN")
+        );
+    }
 
-        // Create the PCN directory
-        create_pcn_dir(pcn_name).unwrap();
+    #[test]
+    fn test_get_pcn_path_on_unix() {
+        env::remove_var("USERPROFILE");
+        assert_eq!(get_pcn_path(), env::current_dir().unwrap().join("PCN"));
+    }
 
-        // Test creating subdirectories that don't already exist
-        let dirs = "dir1,dir2,dir3";
-        let expected_paths = vec![
-            format!("{}/dir1", dir_path),
-            format!("{}/dir2", dir_path),
-            format!("{}/dir3", dir_path),
-        ];
-        assert_eq!(create_subdirs(pcn_name, dirs).unwrap(), expected_paths);
-        assert!(fs::metadata(&expected_paths[0]).unwrap().is_dir());
-        assert!(fs::metadata(&expected_paths[1]).unwrap().is_dir());
-        assert!(fs::metadata(&expected_paths[2]).unwrap().is_dir());
+    #[test]
+    fn test_get_pcn_number() {
+        let input = "123\n";
+        let expected_output = "123";
+        simulate_input(input, || {
+            assert_eq!(get_pcn_number(), expected_output);
+        });
+    }
 
-        // Test creating subdirectories that already exist
-        let dirs = "dir1,dir2,dir3";
-        assert_eq!(create_subdirs(pcn_name, dirs).unwrap(), vec![]);
+    #[test]
+    fn test_get_affected_part_numbers() {
+        let input = "123,456,789\n";
+        let expected_output = vec!["123".to_string(), "456".to_string(), "789".to_string()];
+        simulate_input(input, || {
+            assert_eq!(get_affected_part_numbers(), expected_output);
+        });
+    }
+
+    #[test]
+    fn test_copy_and_rename_file() {
+        let temp_dir = TempDir::new().unwrap();
+        let src_file_path = temp_dir.path().join("PCN_template.xlsx");
+        let dest_dir_path = temp_dir.path().join("123");
+        let dest_file_path = dest_dir_path.join("123.xlsx");
+        let mut src_file = File::create(&src_file_path).unwrap();
+        src_file.write_all(b"test file contents").unwrap();
+        copy_and_rename_file(&src_file_path, &dest_file_path).unwrap();
+        let dest_file_contents = fs::read_to_string(&dest_file_path).unwrap();
+        assert_eq!(dest_file_contents, "test file contents");
+    }
+
+    fn simulate_input<T, F: FnOnce() -> T>(input: &str, f: F) -> T {
+        let mut stdin = io::stdin();
+        let mut buffer = String::new();
+        stdin.read_line(&mut buffer).unwrap();
+        let old_stdin = std::mem::replace(&mut stdin, io::Cursor::new(input));
+        let result = f();
+        std::mem::replace(&mut stdin, old_stdin);
+        result
     }
 }
 
